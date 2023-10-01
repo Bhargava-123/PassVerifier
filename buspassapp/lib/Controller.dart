43e5604bc386f88034ed81958ad4c4255f164e72
Bhargava-123 <bhargavashankar2003@gmail.com>
2023-10-01 09:48:29 +0530

    Log In and Log Out using Shared Pref done

--- buspassapp/lib/Controller.dart
+++ buspassapp/lib/Controller.dart
@@ -5,8 +5,8 @@ var baseURl = "http://127.0.0.1:8000/";
 class ControllerPage extends GetxController {
   late String authToken;
   void setToken(token) {
-    print('*' * 20);
-    print(token);
+    // print('*' * 20);
+    // print(token);
     authToken = 'Bearer $token';
     update();
   }
